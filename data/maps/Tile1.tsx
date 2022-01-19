fd31a658d6563bf7cca453eb7925c2e2ab8c6ddd
Dinosaur <78600651+Darya-Tolmeneva@users.noreply.github.com>
2022-01-19 19:44:21 +0500

    наведен порядок в расположении файлов

---
+++ data/maps/Tile1.tsx
@@ -0,0 +1,55 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.5" tiledversion="1.7.2" name="Tile1" tilewidth="200" tileheight="200" tilecount="17" columns="0">
+ <grid orientation="orthogonal" width="1" height="1"/>
+ <tile id="6">
+  <image width="30" height="30" source="../titles/ice_title.png"/>
+ </tile>
+ <tile id="8">
+  <image width="30" height="30" source="../titles/stone_title.png"/>
+ </tile>
+ <tile id="9">
+  <image width="30" height="30" source="../titles/snow_title.png"/>
+ </tile>
+ <tile id="10">
+  <image width="30" height="30" source="../titles/water_png.png"/>
+ </tile>
+ <tile id="14">
+  <image width="100" height="100" source="../titles/big_home_title.png"/>
+ </tile>
+ <tile id="15">
+  <image width="100" height="100" source="../titles/big_home_title2.png"/>
+ </tile>
+ <tile id="16">
+  <image width="100" height="100" source="../titles/big_home_title3.png"/>
+ </tile>
+ <tile id="17">
+  <image width="100" height="100" source="../titles/big_home_title4.png"/>
+ </tile>
+ <tile id="19">
+  <image width="30" height="30" source="../titles/бочка.png"/>
+ </tile>
+ <tile id="23">
+  <image width="35" height="80" source="../titles/tree_title.png"/>
+ </tile>
+ <tile id="24">
+  <image width="35" height="80" source="../titles/tree_title2.png"/>
+ </tile>
+ <tile id="26">
+  <image width="30" height="30" source="../titles/stone_title2.png"/>
+ </tile>
+ <tile id="27">
+  <image width="30" height="30" source="../titles/stone_title3.png"/>
+ </tile>
+ <tile id="28">
+  <image width="200" height="200" source="../titles/big_home_title5.png"/>
+ </tile>
+ <tile id="5">
+  <image width="30" height="30" source="../titles/box_title.png"/>
+ </tile>
+ <tile id="30">
+  <image width="29" height="28" source="../titles/obsid_tile.png"/>
+ </tile>
+ <tile id="31">
+  <image width="29" height="28" source="../titles/lava_title.png"/>
+ </tile>
+</tileset>
